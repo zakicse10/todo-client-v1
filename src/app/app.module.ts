9f0d01a09d95b22aa462539a32c3a08b11b749db
Zaki Choudhury <zaki.cse.10@gmail.com>
2021-02-07 21:20:54 +0600

    added a new component with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './common/test.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { TodoDetails } from './pages/todo-details/todo-details.component';
+import { TodoAddComponent } from './pages/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TestComponent, TodoListComponent, TodoDetails
+    AppComponent, TestComponent, TodoListComponent, TodoDetails, TodoAddComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule, FormsModule
